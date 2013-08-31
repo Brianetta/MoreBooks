9c72d1bd5953d238cc9e220a9cf668c4bed39f25
Brian Ronald <brian@ppcis.org>
2013-08-31 15:18:13 +0100

    Added GPL header

--- src/com/github/Brianetta/MoreBooks/MoreBooks.java
+++ src/com/github/Brianetta/MoreBooks/MoreBooks.java
@@ -11,12 +11,24 @@ import org.bukkit.plugin.java.JavaPlugin;
 /**
  * Copyright © Brian Ronald
  * 31/08/13
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  */
+
 public class MoreBooks extends JavaPlugin {
 
     @Override
     public void onEnable ()
     {
+        // No explicit startup code at this time
     }
 
     @Override
